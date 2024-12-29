708276e24fb18754a88916bcc84d81681dce9e00
Zeehub333 <zeeinyemen@gmail.com>
2024-12-30 02:41:27 +0300

    modified

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
-pip upgrade
+python -m pip install --upgrade pip
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
